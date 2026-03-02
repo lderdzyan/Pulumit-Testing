1d83034650271d1741aad94eb97b8d5632cac2d6
lderdzyan <lderdzyan@vtgsoftware.com>
2026-03-02 14:38:50 +0400

    stack added

--- infra/index.ts
+++ infra/index.ts
@@ -1,11 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
 import { deployFrontend } from "./frontend";
-
+import { deployBackend } from "./backend";
 
 const stack = pulumi.getStack();
 
 if (stack.startsWith("frontend")) {
   deployFrontend();
-} else {
+} else if(stack.startsWith("backend")){
+  deployBackend();
+}else {
   throw new Error(`Unknown stack naming: ${stack}`);
 }
